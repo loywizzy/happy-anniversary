7dc3302de85180d8787c17fab0541413e3bfb167
loywizzy <kdftvguitarandarmy@gmail.com>
2025-10-30 02:32:23 +0700

    fix: correct typo in HERO_IMAGES array

--- app/config.ts
+++ app/config.ts
@@ -1,7 +1,7 @@
 export const START_DATE = new Date("2023-07-30T00:00:00"); // แก้ได้
 export const ANNIVERSARY_DATE = new Date("2025-10-30T00:00:00"); // แก้ได้
 export const NAMES = "Guitar ♥ On On";
-export const HERO_IMAGES = ["/hero1.JPG", "/hero2.JPG", "/shero3.jpg"];
+export const HERO_IMAGES = ["/hero1.JPG", "/hero2.JPG", "/hero3.jpg"];
 export const GALLERY_IMAGES = [
   "/g1.jpg",
   "/g2.jpg",
